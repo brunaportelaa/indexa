f6d160da901bc6a32d9006aa3600ce811bb972a1
brunaportelaa <brunaporteladj@gmail.com>
2024-10-20 20:40:00 -0300

    Moved git repository to main folder

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import agenda from './agenda.json'
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+  alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.contatos.filter( contato => contato.nome.toLowerCase().startsWith(letra) )
+  }
+
+}
